f2fe450b5d9862183625a05bfed0b7b3a9607b1f
yousef3820 <sasamego893@gmail.com>
2025-11-12 08:21:22 +0200

    Implement Exception interface in ServerException class

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_clean_archticture_myself_2/core/errors/error_model.dart';
 
-class ServerException {
+class ServerException implements Exception {
   final ErrorModel errorModel;
 
   ServerException({required this.errorModel});
